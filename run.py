5343b56236f4dc6e03c4e5ec9a94952e5cd708b7
Abdelrhman Islam <abdelrhman.islam04@gmail.com>
2026-04-29 02:01:30 +0300

    add_run_file

---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from app.models.user_model import db
+from app.routes.auth_routes import auth_bp
+from config import Config
+
+def create_app():
+    app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
+    app.config.from_object(Config)
+
+    # Initialize DB
+    db.init_app(app)
+
+    # Register Blueprints
+    app.register_blueprint(auth_bp)
+
+    return app
+
+
+app = create_app()
+
+# Create tables automatically
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
