3b1f6ee9d0b9db64d11a3e624370c4fc9de9fdc1
Abdelrhman Islam <abdelrhman.islam04@gmail.com>
2026-04-30 02:03:37 +0300

    modify_twofa_service

--- app/services/twofa_service.py
+++ app/services/twofa_service.py
@@ -1,20 +1,20 @@
 import pyotp
 import qrcode
 import io
-import base64
+import base64 # To convert the image to a string so u can send it to the frontend
 
 def generate_2fa_secret():
-    return pyotp.random_base32()
+    return pyotp.random_base32() # This is the secret key that the user will use to generate the 2FA codes
 
-def generate_qr_code(email, secret):
+def generate_qr_code(email, secret): 
     uri = pyotp.totp.TOTP(secret).provisioning_uri(name=email, issuer_name="SecureApp")
 
-    qr = qrcode.make(uri)
-    buffered = io.BytesIO()
-    qr.save(buffered, format="PNG")
+    qr = qrcode.make(uri) 
+    buffered = io.BytesIO() # save in memory 
+    qr.save(buffered, format="PNG") # Convert it to Base64
 
     return base64.b64encode(buffered.getvalue()).decode()
 
-def verify_2fa(secret, code):
+def verify_2fa(secret, code): # Code verification
     totp = pyotp.TOTP(secret)
-    return totp.verify(code)
+    return totp.verify(code) # Compare and return true or false 
